be3c1ed9a190ed829ccc0ac8b71bf92c70d97017
user <fulminazzo21@gmail.com>
2025-05-26 14:40:31 +0200

    Created MockMessagePlugin for testing purposes

---
+++ src/test/java/it/fulminazzo/fulmicommands/MockMessagePlugin.java
@@ -0,0 +1,24 @@
+package it.fulminazzo.fulmicommands;
+
+import it.fulminazzo.fulmicommands.configuration.ConfigurationException;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+public class MockMessagePlugin extends MockPlugin implements FulmiMessagesPlugin {
+    private @Nullable FileConfiguration messages;
+
+    @Override
+    public @NotNull FileConfiguration getMessages() {
+        if (messages == null); //TODO: throw.
+        return messages;
+    }
+
+    @Override
+    public @NotNull FileConfiguration setupMessages() throws ConfigurationException {
+        messages = FulmiMessagesPlugin.super.setupMessages();
+        return messages;
+    }
+
+
+}
