04ed556eea43708fc31d639123bd5e30838702a5
user <fulminazzo21@gmail.com>
2025-05-26 15:08:55 +0200

    Added tests for FulmiMessages methods

---
+++ src/test/groovy/it/fulminazzo/fulmicommands/FulmiMessagesTest.groovy
@@ -0,0 +1,28 @@
+package it.fulminazzo.fulmicommands
+
+import spock.lang.Specification
+
+class FulmiMessagesTest extends Specification {
+
+    void setup() {
+        def plugin = new MockMessagePlugin()
+        plugin.setupMessages()
+        plugin.getMessages().set(Messages.PREFIXED.path, '<prefix> Hello, world!')
+        plugin.getMessages().set(Messages.NO_PREFIX.path, 'Hello, friend!')
+    }
+
+    def 'test that #message returns #expected'() {
+        when:
+        def actual = message.getMessage()
+
+        then:
+        actual == expected
+
+        where:
+        message             || expected
+        Messages.PREFIXED   || Messages.PREFIXED.prefix + ' Hello, world!'
+        Messages.NO_PREFIX  || 'Hello, friend!'
+        Messages.NOT_EXISTS || "Invalid configuration file detected, could not find path: $Messages.NOT_EXISTS.path"
+    }
+
+}
