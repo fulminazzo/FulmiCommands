3119f414a17f5424d578f6675dff0a83f79297e8
user <fulminazzo21@gmail.com>
2025-05-26 14:34:36 +0200

    Created FulmiPlugin interface with getConfiguration and setupConfiguration methods

---
+++ src/main/java/it/fulminazzo/fulmicommands/FulmiPlugin.java
@@ -0,0 +1,42 @@
+package it.fulminazzo.fulmicommands;
+
+import it.fulminazzo.fulmicommands.configuration.ConfigurationException;
+import it.fulminazzo.fulmicommands.configuration.Configurator;
+import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+
+/**
+ * A basic implementation of a FulmiCommands plugin.
+ */
+public interface FulmiPlugin {
+
+    /**
+     * Gets the configuration.
+     *
+     * @return the configuration
+     */
+    @NotNull FileConfiguration getConfiguration();
+
+    /**
+     * Sets up the <b>config.yml</b> configuration file.
+     *
+     * @return the configuration
+     * @throws ConfigurationException in case of any errors
+     */
+    default @NotNull FileConfiguration setupConfiguration() throws ConfigurationException {
+        return Configurator.newBuilder()
+                .pluginDirectory(getPluginDirectory())
+                .name("config")
+                .build();
+    }
+
+    /**
+     * Gets the plugin directory.
+     *
+     * @return the plugin directory
+     */
+    @NotNull File getPluginDirectory();
+
+}
