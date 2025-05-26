8622ec0f3a7b24b72392d9807f8227d56f1497df
user <fulminazzo21@gmail.com>
2025-05-26 15:04:28 +0200

    Added getPlugin function

--- src/test/java/it/fulminazzo/fulmicommands/MockMessagePlugin.java
+++ src/test/java/it/fulminazzo/fulmicommands/MockMessagePlugin.java
@@ -2,11 +2,19 @@ package it.fulminazzo.fulmicommands;
 
 import it.fulminazzo.fulmicommands.configuration.ConfigurationException;
 import it.fulminazzo.yamlparser.configuration.FileConfiguration;
+import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 public class MockMessagePlugin extends MockPlugin implements FulmiMessagesPlugin {
+    @Getter
+    private static MockMessagePlugin plugin;
+
     private @Nullable FileConfiguration messages;
+
+    public MockMessagePlugin() {
+        plugin = this;
+    }
 
     @Override
     public @NotNull FileConfiguration getMessages() {
